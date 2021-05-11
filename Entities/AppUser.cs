51aa5a80ca57ec4390c43a1ab34f425f539ff257
Heisonnavarro <69168686+Heisonnavarro@users.noreply.github.com>
2021-05-10 18:41:12 -0600

    Final seccion 2

---
+++ Entities/AppUser.cs
@@ -0,0 +1,17 @@
+namespace Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        // implementacion completa de una propiedad o varaible , la forma anterior es la manera corta
+        // private int myVar;
+        // public int MyProperty
+        // {
+        //     get { return myVar; }
+        //     set { myVar = value; }
+        // }
+        
+    }
+}
